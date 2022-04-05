09370ca9fd66d0e9b61d4a3d6bfcef295f4d78a3
Martin Okwuegbe <Martin.Okwuegbe@gmail.com>
2022-04-05 00:58:32 -0400

    pushing my files

---
+++ script.js
@@ -0,0 +1,16 @@
+    let oddID = [];
+    let evenID = [];
+
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 2 === 0)  {
+        evenID.push(i);
+     }
+     else {
+         oddID.push(i);
+}
+}
+console.log(oddID);
+console.log(evenID);
+
